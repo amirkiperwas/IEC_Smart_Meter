4cb936d295362a8218177df743c8eba0f50ce2d5
Amir Kiperwas <samirkip@gmail.com>
2023-11-16 13:13:07 +0200

    Update analyze_smart_meter_readings.py

--- analyze_smart_meter_readings.py
+++ analyze_smart_meter_readings.py
@@ -15,7 +15,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from docopt import docopt
 
-__version__ = '1.0.0'
+__version__ = '1.0.01'
 __author__ = 'Amir Kiperwas'
 
 # constants
@@ -25,23 +25,27 @@ KWH_PRICE_NIS = 0.48*1.17
 PLANS = []
 PLANS.append({'plan name': 'PazGaz flat discount', 'percent discount': 5, 'from weekday': 0, 'to weekday': 6,
               'from hour': '00:00', 'to hour': '23:59'})
-PLANS.append({'plan name': 'PazGaz day discount', 'percent discount': 15, 'from weekday': 0, 'to weekday': 6,
+PLANS.append({'plan name': 'PazGaz day discount', 'percent discount': 15, 'from weekday': 6, 'to weekday': 3,
               'from hour': '08:00', 'to hour': '15:59'})
 PLANS.append({'plan name': 'PazGaz Weekend discount', 'percent discount': 10, 'from weekday': 4, 'to weekday': 5,
               'from hour': '00:00', 'to hour': '23:59'})
-PLANS.append({'plan name': 'PazGaz Night discount', 'percent discount': 15, 'from weekday': 0, 'to weekday': 6,
-              'from hour': '23:00', 'to hour': '06:59'})
-PLANS.append({'plan name': 'Electra flat discount', 'percent discount': 7, 'from weekday': 0, 'to weekday': 6,
+PLANS.append({'plan name': 'PazGaz Night discount', 'percent discount': 15, 'from weekday': 6, 'to weekday': 3,
+              'from hour': '00:00', 'to hour': '06:59'})
+
+PLANS.append({'plan name': 'Electra Power flat discount', 'percent discount': 7, 'from weekday': 0, 'to weekday': 6,
               'from hour': '00:00', 'to hour': '23:59'})
-PLANS.append({'plan name': 'Electra Hi-Tec discount', 'percent discount': 10, 'from weekday': 0, 'to weekday': 6,
+PLANS.append({'plan name': 'Electra Power Hi-Tec discount', 'percent discount': 10, 'from weekday': 0, 'to weekday': 6,
               'from hour': '23:00', 'to hour': '16:59'})
+PLANS.append({'plan name': 'Electra Power Night discount', 'percent discount': 20, 'from weekday': 0, 'to weekday': 6,
+              'from hour': '23:00', 'to hour': '6:59'})
+
 PLANS.append({'plan name': 'Cellcom Nights discount', 'percent discount': 20, 'from weekday': 6, 'to weekday': 3,
               'from hour': '22:00', 'to hour': '06:59'})
-PLANS.append(
-    {'plan name': 'Cellcom Work from home discount', 'percent discount': 15, 'from weekday': 6, 'to weekday': 3,
+PLANS.append({'plan name': 'Cellcom Work from home discount', 'percent discount': 15, 'from weekday': 6, 'to weekday': 3,
      'from hour': '07:00', 'to hour': '16:59'})
 PLANS.append({'plan name': 'Cellcom flat discount', 'percent discount': 7, 'from weekday': 0, 'to weekday': 6,
               'from hour': '00:00', 'to hour': '23:59'})
+
 PLANS.append({'plan name': 'Amisragas flat discount', 'percent discount': 6.5, 'from weekday': 0, 'to weekday': 6,
               'from hour': '00:00', 'to hour': '23:59'})
 
@@ -77,7 +81,7 @@ def get_meter_data_from_csv(csv_file_name):
     return meter_data
 
 
-if __name__ == '__main__':
+def main():
     args = docopt(__doc__)
     args_str = str(args)
     args_str = args_str.replace('\n','')
@@ -92,7 +96,9 @@ if __name__ == '__main__':
 
     total_no_discount = data['Consumption, kWh'].sum()*KWH_PRICE_NIS
 
-    print('total without dicount: ', total_no_discount, ' NIS')
+    print('Total without dicount: ', "{:.2f}".format(total_no_discount), ' NIS')
+    results = []
+
     for plan in PLANS:
         plan_name = plan['plan name']
         if plan['from weekday'] <= plan['to weekday'] :
@@ -106,12 +112,17 @@ if __name__ == '__main__':
         df = df.reset_index()
         new_total = total_no_discount - discounted * plan['percent discount'] / 100;
         saving_pecent = (discounted * plan['percent discount'] / 100 / total_no_discount)*100;
+        results.append({'plan name': plan_name, 'total with discount': new_total, 'savings': total_no_discount-new_total , 'savings %': saving_pecent})
         print(plan['plan name'], 'new total : %d NIS a saving of %.2f %%' % (new_total, saving_pecent))
+
+    max_savings_plan = max(results, key=lambda x:x['savings'])
+    print('\nBest plan for you: plan "', max_savings_plan['plan name'], '" would have saved you', "{:.2f}".format(max_savings_plan['savings']), ' NIS','which is', "{:.2f}".format(max_savings_plan['savings %']), '%')
 
     if not no_graph_flag:
         data_pivot = data.drop(['Interval starting', 'Interval starting date'], axis=1)
         data_pivot['Interval starting time'] = data_pivot['Interval starting time'].dt.strftime('%H:%M')
         data_pivot = map_days_of_week(data_pivot, 'day_of_week')
+    #    data_pivot = pd.pivot_table(data, index='Interval starting time', values='Consumption, kWh', aggfunc='sum')
         data_pivot = pd.pivot_table(data_pivot, index='Interval starting time', values='Consumption, kWh', columns='day_of_week', aggfunc='sum')
         cols = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
         data_pivot = data_pivot[cols]
@@ -132,3 +143,6 @@ if __name__ == '__main__':
         plt.pause(0.01)
         input("Press enter to continue...")
         plt.close()
+
+if __name__ == "__main__":
+    main()
